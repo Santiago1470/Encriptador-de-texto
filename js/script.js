44a2f2d4e25543bb17680135353fd684311add41
Santiago1470 <st147086@gmail.com>
2024-07-12 23:18:15 -0500

    Dando animación a mensaje de error
    Se creó la función mostrarError() y ocultarError(). Finalmente, se creó la función desencriptar(), pero solo la parte inicial de la lógica

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ var containerNotFound = document.querySelector('#container-notFound');
 var btnLimpiar = document.querySelector('#btn-limpiar');
 btnLimpiar.addEventListener('click', limpiar);
 
-function encriptar(){
+function encriptar() {
     let textoEntrada = txtaEntrada.value;
     let letras = textoEntrada.split('');
     let resultado = '';
@@ -26,6 +26,27 @@ function encriptar(){
     containerNotFound.style.display = 'none';
     txtaResultado.style.display = 'block';
     txtaResultado.innerHTML = resultado;
+    desencriptar()
+}
+
+function desencriptar() {
+    let textoEntrada = txtaResultado.textContent;
+    let letras = textoEntrada.split(' ');
+    let resultado = '';
+
+    letras.forEach(p => {
+        // resultado += (llaves.has(p)) ? llaves.get(p) : p;
+        for (let [clave, valor] of llaves.entries()) {
+            // if (p.includes(valor)) {
+                
+            // }
+            console.log(p.indexOf(valor))
+        }
+    });
+    
+    // containerNotFound.style.display = 'none';
+    // txtaResultado.style.display = 'block';
+    // txtaResultado.innerHTML = resultado;
 }
 
 function limpiar() {
@@ -33,4 +54,24 @@ function limpiar() {
     txtaResultado.style.display = 'none';
     txtaResultado.innerHTML = '';
     txtaEntrada.value = '';
+}
+
+var contenedorError = document.getElementById('contendorError');
+
+function mostrarError() {
+    let pixeles = -530;
+    let mover = setInterval(() => {
+        pixeles += (pixeles < 0) ? 5 : clearInterval(mover);
+        // console.log(pixeles);
+        contenedorError.style.left = `${pixeles}px`;
+    }, 8);
+}
+
+function ocultarError() {
+    let pixeles = contenedorError.getBoundingClientRect().x;
+    let mover = setInterval(() => {
+        pixeles += (pixeles >= -530) ? -5 : clearInterval(mover);
+        // console.log(pixeles);
+        contenedorError.style.left = `${pixeles}px`;
+    }, 8);
 }
