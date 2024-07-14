7065d1594b2d0db8a2b51f9c4c5670ce2f725d4e
Santiago1470 <st147086@gmail.com>
2024-07-13 22:35:49 -0500

    Función de desencriptar y copiar terminada

--- js/script.js
+++ js/script.js
@@ -6,13 +6,19 @@ llaves.set('o', 'ober');
 llaves.set('u', 'ufat');
 
 var txtaEntrada = document.getElementById('txta-entrada');
+
 var btnEncriptar = document.querySelector("#btn-encriptar");
 btnEncriptar.addEventListener("click", encriptar);
+
+var btnDesencriptar = document.querySelector('#btn-desencriptar');
+btnDesencriptar.addEventListener("click", desencriptar);
 
 var txtaResultado = document.getElementById('txta-resultado');
 var containerNotFound = document.querySelector('#container-notFound');
 var btnLimpiar = document.querySelector('#btn-limpiar');
 btnLimpiar.addEventListener('click', limpiar);
+var btnCopiar = document.querySelector("#btn-copiar");
+btnCopiar.addEventListener('click', copiar);
 
 function encriptar() {
     let textoEntrada = txtaEntrada.value;
@@ -22,31 +28,47 @@ function encriptar() {
     letras.forEach(p => {
         resultado += (llaves.has(p)) ? llaves.get(p) : p;
     });
-    
+
     containerNotFound.style.display = 'none';
     txtaResultado.style.display = 'block';
     txtaResultado.innerHTML = resultado;
-    desencriptar()
 }
 
 function desencriptar() {
-    let textoEntrada = txtaResultado.textContent;
+    let textoEntrada = txtaEntrada.value;
     let letras = textoEntrada.split(' ');
     let resultado = '';
 
     letras.forEach(p => {
-        // resultado += (llaves.has(p)) ? llaves.get(p) : p;
         for (let [clave, valor] of llaves.entries()) {
-            // if (p.includes(valor)) {
-                
-            // }
-            console.log(p.indexOf(valor))
+            let regex = new RegExp(`${valor}`, "g");
+            p = p.replace(regex, clave)
         }
+        resultado += p + " ";
     });
-    
-    // containerNotFound.style.display = 'none';
-    // txtaResultado.style.display = 'block';
-    // txtaResultado.innerHTML = resultado;
+
+    containerNotFound.style.display = 'none';
+    txtaResultado.style.display = 'block';
+    txtaResultado.innerHTML = resultado;
+}
+
+function copiar() {
+    const div = txtaResultado;
+    const selection = window.getSelection();
+    const range = document.createRange();
+    range.selectNodeContents(div); // Selecciona el contenido del div
+    selection.removeAllRanges(); // Limpia cualquier selección anterior
+    selection.addRange(range); // Añade la nueva selección
+
+    // Copia el texto al portapapeles
+    navigator.clipboard.writeText(selection.toString()).then(() => {
+        alert('Texto copiado: ' + selection.toString());
+    }).catch(err => {
+        console.error('Error al copiar: ', err);
+    });
+
+    // Limpia la selección
+    selection.removeAllRanges();
 }
 
 function limpiar() {
